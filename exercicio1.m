41de63a8e012049d9734933538d0c2ac5e0ef5de
luizamourmouris <luizamourmouris@gmail.com>
2025-09-01 14:20:54 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -6,10 +6,19 @@ imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+h= zeros(imax, 1);
+h(1)= x0;
+erro= zeros(length(h), 1);
 
-% digite seu codigo aqui
-t = 0; % alterar
-
+for ii = 1:length(h)-1
+  if ii~=1
+    erro (ii)= abs((h(ii)-h(ii-1))/h(ii))
+    if erro (ii)< es
+      break
+    endif
+  endif
+  h(ii+1)=h(ii)-func(h(ii))/func_d(h(ii))
+endfor
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
